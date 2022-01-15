6cd03da21cecbfd7e6aaef70f451458137779dd5
sreejithmahesanpersonal <sreejith.mahesan@gmail.com>
2022-01-15 12:28:02 +0530

    Initial Template Cucumber-Selenium

---
+++ src/main/java/com/sreejithmahesan/App.java
@@ -0,0 +1,13 @@
+package com.sreejithmahesan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
